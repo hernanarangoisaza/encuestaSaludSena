f9404138df188052637ad9b6b816d6e7f657b560
Hernán Arango Isaza <31634784+hernanarangoisaza@users.noreply.github.com>
2020-11-26 12:19:17 -0500

    ajustes

--- app/index.php
+++ app/index.php
@@ -1,45 +1,53 @@
-
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Select CRUD pages</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <style type="text/css">
-        .page-header h2{
-            margin-top: 0;
-        }
-        table tr td:last-child a{
-            margin-right: 5px;
-        }
-    </style>
-</head>
-<body>
-<fieldset>
-<center>
-<legend>Available CRUD pages</legend>
-<div class="form-group">
-    <a href="aprendices-index.php" class="btn btn-primary" role="button">Aprendices</a> 
-	<a href="centros_formacion-index.php" class="btn btn-primary" role="button">Centros de formación</a> 
-	<a href="departamentos-index.php" class="btn btn-primary" role="button">Departamentos</a> 
-	<a href="encuesta_signos-index.php" class="btn btn-primary" role="button">Encuesta de signos</a> 
-	<a href="fichas_formacion-index.php" class="btn btn-primary" role="button">Fichas de formación</a> 
-	<a href="horarios-index.php" class="btn btn-primary" role="button">Horarios</a> 
-	<a href="municipios-index.php" class="btn btn-primary" role="button">Municipios</a> 
-	<a href="preguntas_encuesta-index.php" class="btn btn-primary" role="button">Preguntas de la encuesta</a> 
-	<a href="programas_formacion-index.php" class="btn btn-primary" role="button">Programas de formación</a> 
-	<a href="respuestas_encuesta-index.php" class="btn btn-primary" role="button">Respuestas de la encuesta</a> 
-	<a href="roles_sistema-index.php" class="btn btn-primary" role="button">Roles del sistema</a> 
-	<a href="tipos_generos-index.php" class="btn btn-primary" role="button">Tipos de géneros</a> 
-	<a href="tipos_identificacion-index.php" class="btn btn-primary" role="button">Tipos de identificación</a> 
-	<a href="tomas_temperatura-index.php" class="btn btn-primary" role="button">Tomas de temperatura</a> 
-	<a href="usuarios-index.php" class="btn btn-primary" role="button">Usuarios del sistema</a> 
-	<a href="vinculaciones_sena-index.php" class="btn btn-primary" role="button">Tipos de vinculaciones</a> 
-	
-</div>
-</center>
-</fieldset>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Sistema de encuestas para registro de síntomas y temperatura en el SENA - CDITI</title>
+        <link rel="stylesheet" href="xhttps://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+        <link rel="stylesheet" href="css/estilos.css" />
+    </head>
+    <body>
+        <div class="menu">
+            <div class="title">MENÚ</div>
+            <ul class="nav">
+
+                <li class=""><a href="encuesta_signos-index.php" class="">Encuesta de signos</a></li>
+                <li class=""><a href="tomas_temperatura-index.php" class="">Tomas de temperatura</a></li>
+                <li class=""><a href="preguntas_encuesta-index.php" class="">Preguntas de la encuesta</a></li>
+                <li class=""><a href="respuestas_encuesta-index.php" class="">Respuestas de la encuesta</a></li>
+
+                <li class=""><a href="aprendices-index.php" class="">Aprendices</a></li>
+
+                <li class=""><a href="centros_formacion-index.php" class="">Centros de formación</a></li>
+                <li class=""><a href="programas_formacion-index.php" class="">Programas de formación</a></li>
+                <li class=""><a href="fichas_formacion-index.php" class="">Fichas de formación</a></li>
+                <li class=""><a href="horarios-index.php" class="">Horarios</a></li>
+
+                <li class=""><a href="departamentos-index.php" class="">Departamentos</a></li>
+                <li class=""><a href="municipios-index.php" class="">Municipios</a></li>
+
+                <li class=""><a href="tipos_generos-index.php" class="">Tipos de géneros</a></li>
+                <li class=""><a href="tipos_identificacion-index.php" class="">Tipos de identificación</a></li>
+                <li class=""><a href="vinculaciones_sena-index.php" class="">Tipos de vinculaciones</a></li>
+
+                <li class=""><a href="usuarios-index.php" class="">Usuarios del sistema</a></li>
+                <li class=""><a href="roles_sistema-index.php" class="">Roles del sistema</a></li>
+            </ul>
+        </div>
+
+        <div class="intro">
+            SENA - CDITI<br />
+            Gestión de encuestas para registro de síntomas y temperatura<br />
+            COVID-19
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <script src="js/libreria.js" type="text/javascript"></script>
+
+        <script>
+            $(document).ready(function () {});
+        </script>
+
+    </body>
 </html>
